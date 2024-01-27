6535717f3d1d7249769f17198e0d906930b13771
liushuai <ls2001927@sohu.com>
2024-01-27 20:20:22 +0800

    update pipeline

--- basic_move/pipeline.js
+++ basic_move/pipeline.js
@@ -7,7 +7,7 @@ const angles = [0, 90, 180, 270];
 const delay = 1000; // 1秒
 let can_see = false;
 
-function walk() {
+async function walk() {
   return new Promise((resolve) => {
     bot.setControlState('forward', true); // 开始向前移动
     setTimeout(() => {
@@ -17,7 +17,7 @@ function walk() {
   });
 }
 
-function lookAndChatWithDelay() {
+async function lookAndChatWithDelay() {
   return new Promise(async (resolve) => {
     let index = 0;
 
@@ -44,30 +44,82 @@ function lookAndChatWithDelay() {
 
     await nextRound();
     if (can_see==true){
-      can_see==false //reset to false
-      bot.chat("can see")
+      can_see=false //reset to false
+      bot.chat(`I will start to dig the ${block.name}`)
+      await mineBlock(bot, block.name, 1);
     }
   });
 }
+async function walk() {
+  return new Promise((resolve) => {
+    bot.setControlState('forward', true); // 开始向前移动
+    setTimeout(() => {
+      bot.setControlState('forward', false); // 停止向前移动
+      resolve(); // 异步操作完成后解析 Promise
+    }, 5000);
+  });
+}
 async function reset() {
-  return new Promise((resolve, reject) => {
-    bot.chat('/execute as bot at @s run tp @s -270 64 251', (err) => {
-      if (err) {
-        reject(err); // 如果传送命令出错，拒绝 Promise
-      } else {
-        resolve(); // 传送成功，解决 Promise
-      }
+  return new Promise((resolve) => {
+    bot.chat('/tp @s -270 64 251 0 0', () => {
+      setTimeout(() => {
+        resolve(); // 异步操作完成后解析 Promise
+      }, 1000);
     });
   });
 }
 
-async function main() {
-  bot.chat("start to perceive")  
-  await lookAndChatWithDelay(); // 调用函数开始执行
-  bot.chat("start to walk");
-  await walk();
-  bot.chat("walk completed");
-  reset()
+async function wanderAndClearPathfinder() {
+  return new Promise((resolve) => {
+    const x =
+      bot.entity.position.x +
+      Math.floor(Math.random() * 20 + 10);
+    const y =
+      bot.entity.position.y 
+      // +Math.floor(Math.random() * 20 + 10);
+    const z =
+      bot.entity.position.z 
+      // +Math.floor(Math.random() * 20 + 10);
+    let goal = new GoalNear(x, y, z);
+    bot.pathfinder.setGoal(goal);
+    setTimeout(() => {
+      // 停止向前移动
+      resolve(); // 异步操作完成后解析 Promise
+      bot.pathfinder.setGoal(null); // 清空 pathfinder 的目标
+    }, 5000); // 10 秒
+  });
 }
 
+function wander() {
+  return new Promise((resolve) => {
+    bot.setControlState('forward', true);
+    const base_ori = 0;
+    const min = -90; // 最小望向角度
+    const max = 90;  // 最大望向角度
+    const random_ori = Math.floor(Math.random() * (max - min + 1)) + min;
+    bot.chat(`looking at ${base_ori + random_ori}`);
+    bot.look(base_ori+random_ori,0);
+    setTimeout(() => {
+      bot.setControlState('forward', false); // 停止向前移动
+      resolve(); // 异步操作完成后解析 Promise
+    }, 10000);
+  });
+}
+
+
+async function main() {
+  bot.chat("start to perceive1");
+  await lookAndChatWithDelay();
+  bot.chat("start to walk2");
+  await wanderAndClearPathfinder();
+  // await walk();
+  bot.chat("start to perceive3");
+  await lookAndChatWithDelay(); // 调用函数开始执行
+  bot.chat("start to walk4");
+  await wanderAndClearPathfinder();
+  bot.chat("start to perceive5")
+  await lookAndChatWithDelay();
+  bot.chat("walk completed6");
+  // await reset()
+}
 main();
--- run.py
+++ run.py
@@ -12,7 +12,7 @@ azure_login = {
 from voyager.agents.choiszt_keyboard import change_gamemode
 change_gamemode("spectator")
 voyager = Voyager(
-    mc_port=36581,
+    mc_port=44487,
     openai_api_key=openai.api_key,
 )
 
